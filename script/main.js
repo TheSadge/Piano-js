162bcb123c513fe747056c90731acfd96434c904
Sajjad Hakemzade <149905781+TheSadge@users.noreply.github.com>
2024-08-13 17:48:06 +0330

    main.js

--- script/main.js
+++ script/main.js
@@ -4,14 +4,11 @@ const note = document.querySelector(".nowplaying");
 // Select all the keys that will interact with the user
 const allKeys = document.querySelectorAll(".keys");
 
-// Select all the hint elements for setting transition delays
-const hints = document.querySelectorAll(".hints");
-
 // Add an event listener for keydown events
 window.addEventListener("keydown", function (event) {
   // Find the key element corresponding to the pressed key
   const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
-  if (!key) return; // Exit if no corresponding key element is found
+  if (key != key) return; // Exit if no corresponding key element is found
 
   // Find the audio element corresponding to the pressed key
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
@@ -39,13 +36,6 @@ function removeTransition(event) {
 allKeys.forEach((key) =>
   key.addEventListener("transitionend", removeTransition)
 );
-
-// Set transition delays for hint elements, staggered by 50ms
-hints.forEach(function (element, index) {
-  element.style = `transition-delay: ${index * 50}ms`;
-});
-
-
 
 //
 ////////////////////////////////////////////////////////////////////////////
